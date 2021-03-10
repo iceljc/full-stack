cbfa0e1faa99583e00095fc88d29bd0730ee2035
iceljc <46039045+iceljc@users.noreply.github.com>
2021-03-09 19:26:00 -0600

    Add files via upload

---
+++ js-assignment-jichenglu/js/eventHandling.js
@@ -0,0 +1,57 @@
+﻿function addition() {
+    let a = document.querySelector("#txtFirstNumber").value;
+    let b = document.querySelector("#txtSecondNumber").value;
+    let c = parseFloat(a) + parseFloat(b);
+    document.querySelector("#txtResult").value = c;
+}
+
+function subtraction() {
+    let a = document.querySelector("#txtFirstNumber").value;
+    let b = document.querySelector("#txtSecondNumber").value;
+    let c = parseFloat(a) - parseFloat(b);
+    document.querySelector("#txtResult").value = c;
+}
+
+function multiplication() {
+    let a = document.querySelector("#txtFirstNumber").value;
+    let b = document.querySelector("#txtSecondNumber").value;
+    let c = parseFloat(a) * parseFloat(b);
+    document.querySelector("#txtResult").value = c;
+}
+
+
+function division() {
+    let a = document.querySelector("#txtFirstNumber").value;
+    let b = document.querySelector("#txtSecondNumber").value;
+    let c = parseFloat(a) / parseFloat(b);
+    document.querySelector("#txtResult").value = c;
+}
+
+
+function arithematic(callback) {
+    callback();
+}
+
+
+function calculation() {
+    let operation = document.querySelector("#drpOptions").value;
+
+    if (operation == "addition") {
+        arithematic(addition);
+    } else if (operation == "subtraction") {
+        arithematic(subtraction);
+    } else if (operation == "multiplication") {
+        arithematic(multiplication);
+    } else if (operation == "division") {
+        arithematic(division);
+    } else {
+        document.querySelector("#txtResult").value = "no value";
+    }
+        
+
+}
+
+function countChars() {
+    let a = document.querySelector("#txtAreaMsg").value.length;
+    document.querySelector("#lblmsg").innerText = a + "/10";
+}
